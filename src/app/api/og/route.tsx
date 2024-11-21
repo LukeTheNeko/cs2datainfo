36345f8a35c557855b7db5c692e28d0306570f4b
LukeTheNeko <lucasneko25@gmail.com>
2024-11-20 23:19:21 -0300

    1

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -36,7 +36,7 @@ export async function GET(request: Request) {
                         position: "relative",
                     }}
                 >
-                    <img
+                    <img 
                         src={imageUrl}
                         alt="og image"
                         style={{
