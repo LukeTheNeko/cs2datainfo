6557d9a37a0a3a3d97913acb51d8a62dfe21eda2
LukeTheNeko <lucasneko25@gmail.com>
2024-11-20 23:08:49 -0300

    try fix domain

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -3,29 +3,23 @@ import { ImageResponse } from "next/og";
 export const runtime = "edge"; // Define o ambiente como Edge Function
 
 export async function GET(request: Request) {
-    // Verifica o IP do cliente para permitir somente o servidor ou IPs confiáveis
-    const clientIp = request.headers.get("X-Forwarded-For") || "";
-
-    // Substitua "Seu_IP" pelo IP ou intervalo de IPs do seu servidor
-    if (clientIp !== "Seu_IP") {
-        return new Response("Not Found", { status: 404 });
-    }
-
-    // Ou alternativamente, você pode usar o Referer para validar a origem
-    const referer = request.headers.get("Referer");
-    if (!referer || !referer.includes("cs2datainfo.vercel.app")) {
-        return new Response("Not Found", { status: 404 });
-    }
-
-    // Obtém os parâmetros da URL
     const { searchParams } = new URL(request.url);
+
     const imageUrl = searchParams.get("image");
 
     if (!imageUrl) {
         return new Response("Image URL is required", { status: 400 });
     }
 
-    // Retorna a imagem com o efeito de ofuscação e texto estilizado
+    // Verifica se o imageUrl pertence ao domínio cs2data.info
+    const allowedDomain = "cs2data.info";
+    const url = new URL(imageUrl);
+
+    if (url.hostname !== allowedDomain) {
+        return new Response("Not Found", { status: 404 });
+    }
+
+    // Retorna a imagem gerada com ofuscação e texto estilizado
     return new ImageResponse(
         (
             <div
@@ -46,7 +40,7 @@ export async function GET(request: Request) {
                         width: "50%",
                         height: "50%",
                         objectFit: "contain",
-                        filter: "blur(8px)", // Aplica o efeito de ofuscamento (blur) à imagem
+                        filter: "blur(8px)", // Aplica o efeito de ofuscamento à imagem
                     }}
                 />
                 <div
