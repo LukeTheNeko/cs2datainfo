849000b196a240670db95200ae2cb2b10dbd0315
LukeTheNeko <lucasneko25@gmail.com>
2024-11-20 22:47:17 -0300

    try fix

---
+++ src/app/api/og/route.tsx
@@ -0,0 +1,55 @@
+import { ImageResponse } from "next/og";
+
+export const runtime = "edge";
+
+export async function GET(request: Request) {
+    const { searchParams } = new URL(request.url);
+
+    const imageUrl = searchParams.get("image") || "";
+
+    if (!imageUrl) {
+        return new Response("Image URL is required", { status: 400 });
+    }
+
+    return new ImageResponse(
+        (
+          <div
+          style={{
+            width: "1200px",
+            height: "630px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "#121212",
+            position: "relative", // Necessário para posicionar o texto
+          }}
+        >
+          <img
+            src={imageUrl}
+            alt="og image"
+            style={{
+              width: "50%",
+              height: "50%",
+              objectFit: "contain",
+            }}
+          />
+          <div
+            style={{
+              position: "absolute",
+              bottom: "10px",
+              right: "10px",
+              color: "white",
+              fontSize: "18px",
+            }}
+          >
+            CS2DATA.info
+          </div>
+        </div>
+        
+        ),
+        {
+            width: 1200,
+            height: 630,
+        },
+    );
+}
