a9f787bc3eb0bf61db8b9978a051c99e54831082
LukeTheNeko <lucasneko25@gmail.com>
2025-02-25 23:15:20 -0300

    v1.1.1

--- src/app/others/agents/page.tsx
+++ src/app/others/agents/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import CardSkins from "@/components/Card/CardSkins";
-import axios from "axios";
 import { useEffect, useState } from "react";
 
 interface Rarity {
@@ -38,11 +37,12 @@ export default function Tournament() {
 
   const fetchStickers = async () => {
     try {
-      const response = await axios.get(
-        "https://api.cs2data.info/en/agents.json",
-      );
-      const filteredStickers: Agent[] = response.data.filter(
-        (Agent: Agent) => Agent.market_hash_name !== null,
+      const response = await fetch("https://api.cs2data.info/en/agents.json");
+      const data = await response.json();
+      
+      // Filtra os stickers que têm market_hash_name não nulo
+      const filteredStickers: Agent[] = data.filter(
+        (Agent: Agent) => Agent.market_hash_name !== null
       );
 
       setStickers(filteredStickers);
@@ -67,131 +67,129 @@ export default function Tournament() {
   const totalPages = Math.ceil(orderedStickers.length / stickersPerPage);
   const currentStickers = orderedStickers.slice(
     (currentPage - 1) * stickersPerPage,
-    currentPage * stickersPerPage,
+    currentPage * stickersPerPage
   );
 
   return (
-    <>
-      <div className="bg-zinc-900">
-        <div className="flex justify-center my-10 gap-10 items-center">
-          <div className="flex flex-col text-white font-medium px-6 gap-1">
-            <span className="text-4xl">All Agents</span>
-          </div>
+    <div className="bg-zinc-900">
+      <div className="flex justify-center my-10 gap-10 items-center">
+        <div className="flex flex-col text-white font-medium px-6 gap-1">
+          <span className="text-4xl">All Agents</span>
         </div>
-
-        {/* Nav */}
-        <div className="flex items-center justify-center my-4 text-white py-5">
-          {currentPage > 1 && (
-            <button
-              onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
-              className="mx-2 px-4 py-2 rounded bg-black-300 text-black hover:bg-indigo-800"
-            >
-              {"<"}
-            </button>
-          )}
-
-          {Array.from({ length: 6 }, (_, index) => {
-            const pageNumber = index + 1;
-            const displayPageNumber =
-              currentPage > 3 ? currentPage - 3 + index : pageNumber;
-            return (
-              displayPageNumber <= totalPages && (
-                <button
-                  key={displayPageNumber}
-                  onClick={() => setCurrentPage(displayPageNumber)}
-                  className={`mx-1 px-4 py-2 rounded ${
-                    currentPage === displayPageNumber
-                      ? "bg-indigo-600 text-white"
-                      : "bg-black-300 text-black hover:bg-indigo-800"
-                  }`}
-                >
-                  {displayPageNumber}
-                </button>
-              )
-            );
-          })}
-
-          {currentPage < totalPages && (
-            <button
-              onClick={() =>
-                setCurrentPage((prev) => Math.min(prev + 1, totalPages))
-              }
-              className="mx-2 px-4 py-2 rounded bg-black-300 text-white hover:bg-indigo-800"
-            >
-              {">"}
-            </button>
-          )}
-        </div>
-        {/* Nav */}
-
-        <div className="flex justify-center items-center my-4">
-          <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 justify-center items-center">
-            {currentStickers.length > 0 ? (
-              currentStickers.map((Agent) => (
-                <CardSkins
-                  key={Agent.id}
-                  skinName={Agent.name}
-                  imageUrl={Agent.image}
-                  rarity={Agent.rarity}
-                  specialOption="Default"
-                  priceWithoutStatTrak="no data"
-                  priceWithStatTrak=""
-                  collectionName={Agent.collections[0]?.name || "Unknown Crate"}
-                  collectionImageUrl={Agent.collections[0]?.image || ""}
-                  basePath="/others/agents"
-                />
-              ))
-            ) : (
-              <div className="bg-zinc-900"></div>
-            )}
-          </div>
-        </div>
-
-        {/* Nav */}
-        <div className="flex items-center justify-center my-4 text-white py-5">
-          {currentPage > 1 && (
-            <button
-              onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
-              className="mx-2 px-4 py-2 rounded bg-black-300 text-black hover:bg-indigo-800"
-            >
-              {"<"}
-            </button>
-          )}
-
-          {Array.from({ length: 6 }, (_, index) => {
-            const pageNumber = index + 1;
-            const displayPageNumber =
-              currentPage > 3 ? currentPage - 3 + index : pageNumber;
-            return (
-              displayPageNumber <= totalPages && (
-                <button
-                  key={displayPageNumber}
-                  onClick={() => setCurrentPage(displayPageNumber)}
-                  className={`mx-1 px-4 py-2 rounded ${
-                    currentPage === displayPageNumber
-                      ? "bg-indigo-600 text-white"
-                      : "bg-black-300 text-black hover:bg-indigo-800"
-                  }`}
-                >
-                  {displayPageNumber}
-                </button>
-              )
-            );
-          })}
-
-          {currentPage < totalPages && (
-            <button
-              onClick={() =>
-                setCurrentPage((prev) => Math.min(prev + 1, totalPages))
-              }
-              className="mx-2 px-4 py-2 rounded bg-black-300 text-white hover:bg-indigo-800"
-            >
-              {">"}
-            </button>
-          )}
-        </div>
-        {/* Nav */}
       </div>
-    </>
+
+      {/* Nav */}
+      <div className="flex items-center justify-center my-4 text-white py-5">
+        {currentPage > 1 && (
+          <button
+            onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+            className="mx-2 px-4 py-2 rounded bg-black-300 text-black hover:bg-indigo-800"
+          >
+            {"<"}
+          </button>
+        )}
+
+        {Array.from({ length: 6 }, (_, index) => {
+          const pageNumber = index + 1;
+          const displayPageNumber =
+            currentPage > 3 ? currentPage - 3 + index : pageNumber;
+          return (
+            displayPageNumber <= totalPages && (
+              <button
+                key={displayPageNumber}
+                onClick={() => setCurrentPage(displayPageNumber)}
+                className={`mx-1 px-4 py-2 rounded ${
+                  currentPage === displayPageNumber
+                    ? "bg-indigo-600 text-white"
+                    : "bg-black-300 text-black hover:bg-indigo-800"
+                }`}
+              >
+                {displayPageNumber}
+              </button>
+            )
+          );
+        })}
+
+        {currentPage < totalPages && (
+          <button
+            onClick={() =>
+              setCurrentPage((prev) => Math.min(prev + 1, totalPages))
+            }
+            className="mx-2 px-4 py-2 rounded bg-black-300 text-white hover:bg-indigo-800"
+          >
+            {">"}
+          </button>
+        )}
+      </div>
+      {/* Nav */}
+
+      <div className="flex justify-center items-center my-4">
+        <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 justify-center items-center">
+          {currentStickers.length > 0 ? (
+            currentStickers.map((Agent) => (
+              <CardSkins
+                key={Agent.id}
+                skinName={Agent.name}
+                imageUrl={Agent.image}
+                rarity={Agent.rarity}
+                specialOption="Default"
+                priceWithoutStatTrak="no data"
+                priceWithStatTrak=""
+                collectionName={Agent.collections[0]?.name || "Unknown Crate"}
+                collectionImageUrl={Agent.collections[0]?.image || ""}
+                basePath="/others/agents"
+              />
+            ))
+          ) : (
+            <div className="bg-zinc-900"></div>
+          )}
+        </div>
+      </div>
+
+      {/* Nav */}
+      <div className="flex items-center justify-center my-4 text-white py-5">
+        {currentPage > 1 && (
+          <button
+            onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+            className="mx-2 px-4 py-2 rounded bg-black-300 text-black hover:bg-indigo-800"
+          >
+            {"<"}
+          </button>
+        )}
+
+        {Array.from({ length: 6 }, (_, index) => {
+          const pageNumber = index + 1;
+          const displayPageNumber =
+            currentPage > 3 ? currentPage - 3 + index : pageNumber;
+          return (
+            displayPageNumber <= totalPages && (
+              <button
+                key={displayPageNumber}
+                onClick={() => setCurrentPage(displayPageNumber)}
+                className={`mx-1 px-4 py-2 rounded ${
+                  currentPage === displayPageNumber
+                    ? "bg-indigo-600 text-white"
+                    : "bg-black-300 text-black hover:bg-indigo-800"
+                }`}
+              >
+                {displayPageNumber}
+              </button>
+            )
+          );
+        })}
+
+        {currentPage < totalPages && (
+          <button
+            onClick={() =>
+              setCurrentPage((prev) => Math.min(prev + 1, totalPages))
+            }
+            className="mx-2 px-4 py-2 rounded bg-black-300 text-white hover:bg-indigo-800"
+          >
+            {">"}
+          </button>
+        )}
+      </div>
+      {/* Nav */}
+    </div>
   );
 }
